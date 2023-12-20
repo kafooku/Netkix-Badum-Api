baee4d0ffb5255ee0c0a0db58a3df7efe207d12c
LEVO <kevinkafuku@levos-mbp.mynetworksettings.com>
2023-12-19 22:11:04 -0500

    Netkix Badum Api Spring Initializr

---
+++ src/main/java/com/kafooku/NetkixBadumApi/NetkixBadumApiApplication.java
@@ -0,0 +1,13 @@
+package com.kafooku.NetkixBadumApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetkixBadumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetkixBadumApiApplication.class, args);
+	}
+
+}
